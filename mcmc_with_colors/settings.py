3723fbffa9d1599460e153522fe5fc6db6cb38d8
Andrew Ang <ang.kunanit@gmail.com>
2015-05-02 01:47:32 -0400

    static configuration

--- mcmc_with_colors/settings.py
+++ mcmc_with_colors/settings.py
@@ -67,13 +67,9 @@ WSGI_APPLICATION = 'mcmc_with_colors.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/New_York'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -96,13 +92,14 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 # import os
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # django bootstrap
 BOOTSTRAP3 = {}
 BOOTSTRAP3['include_jquery'] = True
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,'templates'),)
 
