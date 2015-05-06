7d103d54ce210eac1d425fcd471f79999cae030a
Andrew Ang <ang.kunanit@gmail.com>
2015-05-06 16:30:19 -0400

    settings

--- mcmc_with_colors/settings.py
+++ mcmc_with_colors/settings.py
@@ -8,7 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -71,9 +71,6 @@ DATABASES = {'default': dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-# ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 # import os
